cdd6e18e525955517be8ff323be82180ca72a03a
mma2 <mma2@SPBWS-PRC293.t-systems.ru>
2012-11-21 18:59:21 +0400

    Billing module WIP

--- lib/modules/billing/main.pm
+++ lib/modules/billing/main.pm
@@ -136,194 +136,6 @@ get '/data/list' => sub {
 }~;
 };
 
-post '/data/save' => sub {
-  if (!&taracot::admin::_auth()) { redirect '/admin?'.md5_hex(time); return true }
-  _load_lang();  
-  content_type 'application/json';
-  my $username=param('username') || '';
-  my $password=param('password') || '';
-  my $email=param('email') || '';
-  my $phone=param('phone') || '';
-  my $realname=param('realname') || '';
-  my $status=param('status') || 0;
-  $status=int($status);
-  my $id=param('id') || 0;
-  $id=int($id);
-  
-  if ($username !~ /^[A-Za-z0-9_\-]{1,100}$/) {
-   return qq~{"result":"0","field":"username","error":"~.$lang->{form_error_invalid_username}.qq~"}~;
-  }
-  $username=lc $username;
-  if (($id > 0 && length($password) > 0) || $id eq 0) {
-    if ($password !~ /^[A-Za-z0-9_\-\$\!\@\#\%\^\&\[\]\{\}\*\+\=\.\,\'\"\|\<\>\?]{6,100}$/) {
-     return qq~{"result":"0","field":"password","error":"~.$lang->{form_error_invalid_password}.qq~"}~;
-    }
-  }
-  if ($email !~ /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/ || length($email) >80) {
-   return qq~{"result":"0","field":"email","error":"~.$lang->{form_error_invalid_email}.qq~"}~;
-  }
-  $email=lc $email;
-  $realname=~s/[\<\>\"\'\n\r\\\/]//gm;
-    if ($realname !~ /^.{0,80}$/) {
-   return qq~{"result":"0","field":"realname","error":"~.$lang->{form_error_invalid_realname}.qq~"}~;
-  }
-  if ($status < 0 || $status > 2) {
-   return qq~{"result":"0","error":"~.$lang->{form_error_invalid_status}.qq~"}~;
-  }
-  $phone=~s/[^0-9]//gm;
-  if (length($phone) > 40) {
-   return qq~{"result":"0","error":"~.$lang->{form_error_invalid_phone}.qq~"}~; 
-  }
-  my $dupesql='';
-  if ($id > 0) {
-   $dupesql=' AND id != '.$id;
-  }
-  
-  if ($taracot::admin::authdata->{username} eq $username && $taracot::admin::authdata->{status} eq 2) {
-   $status=2;
-  }
-  
-  my $sth = database->prepare(
-   'SELECT id FROM '.config->{db_table_prefix}.'_billing WHERE username='.database->quote($username).$dupesql
-  );
-  if ($sth->execute()) {
-   my ($tmpid) = $sth->fetchrow_array;
-   if ($tmpid) {
-    $sth->finish();
-    return qq~{"result":"0","field":"username","error":"~.$lang->{form_error_duplicate_username}.qq~"}~;
-   }
-  }
-  $sth->finish();
-  
-  $sth = database->prepare(
-   'SELECT id FROM '.config->{db_table_prefix}.'_billing WHERE email='.database->quote($email).$dupesql
-  );
-  if ($sth->execute()) {
-   my ($tmpid) = $sth->fetchrow_array;
-   if ($tmpid) {
-    $sth->finish();
-    return qq~{"result":"0","field":"email","error":"~.$lang->{form_error_duplicate_email}.qq~"}~;
-   } 
-  }
-  $sth->finish();
-  
-  if ($id > 0) {
-   if ($password) {
-    $password = md5_hex(config->{salt}.$password);
-    database->quick_update(config->{db_table_prefix}.'_billing', { id => $id }, { username => $username, password => $password, email => $email, phone => $phone, realname => $realname, status => $status, lastchanged => time });
-   } else {
-    database->quick_update(config->{db_table_prefix}.'_billing', { id => $id }, { username => $username, email => $email, phone => $phone, realname => $realname, status => $status, lastchanged => time });
-   }
-  } else {   
-   database->quick_insert(config->{db_table_prefix}.'_billing', { username => $username, password => $password, email => $email, phone => $phone, realname => $realname, status => $status, lastchanged => time });
-  }
-      
-  return qq~{"result":"1"}~;
-};
-
-post '/data/save/field' => sub {
-  if (!&taracot::admin::_auth()) { redirect '/admin?'.md5_hex(time); return true }
-  _load_lang();
-  content_type 'application/json';
-
-  my $field_name=param('field_name') || '';
-  my $field_id=param('field_id') || 0;
-  $field_id=int($field_id);
-  my $field_value=param('field_value') || '';
-
-  # Pre-check all fields
-  
-  if ($field_name ne 'username' && $field_name ne 'realname' && $field_name ne 'email' && $field_name ne 'status' && $field_name ne 'phone' ) {
-   return '{"result":"0", "error":"'.$lang->{field_edit_error_unknown}.'"}'; 
-  }
-  
-  # Check username
-  
-  if ($field_name eq 'username') {
-   my $username=$field_value;
-   if ($username !~ /^[A-Za-z0-9_\-]{1,100}$/) {
-    return qq~{"result":"0","error":"~.$lang->{form_error_invalid_username}.qq~"}~;
-   }
-   $username=lc $username;
-   my $sth = database->prepare(
-    'SELECT id FROM '.config->{db_table_prefix}.'_billing WHERE username='.database->quote($username).' AND id != '.$field_id
-   );
-   if ($sth->execute()) {
-    my ($tmpid) = $sth->fetchrow_array;
-    if ($tmpid) {
-     $sth->finish();
-     return qq~{"result":"0","error":"~.$lang->{form_error_duplicate_username}.qq~"}~;
-    }
-   }
-   $sth->finish();
-   database->quick_update(config->{db_table_prefix}.'_billing', { id => $field_id }, { username => $username, lastchanged => time });
-   return qq~{"result":"1"}~;
-  }
-  
-  # Check e-mail
-  
-  if ($field_name eq 'email') {
-   my $email=$field_value;
-   if ($email !~ /^[A-Za-z0-9_\-\$\!\@\#\%\^\&\[\]\{\}\*\+\=\.\,\'\"\|\<\>\?]{6,100}$/ || length($email) > 80) {
-    return qq~{"result":"0","error":"~.$lang->{form_error_invalid_email}.qq~"}~;
-   }
-   $email=lc $email;
-   my $sth = database->prepare(
-    'SELECT id FROM '.config->{db_table_prefix}.'_billing WHERE email='.database->quote($email).' AND id != '.$field_id
-   );
-   if ($sth->execute()) {
-    my ($tmpid) = $sth->fetchrow_array;
-    if ($tmpid) {
-     $sth->finish();
-     return qq~{"result":"0","error":"~.$lang->{form_error_duplicate_email}.qq~"}~;
-    }
-   }
-   $sth->finish();
-   database->quick_update(config->{db_table_prefix}.'_billing', { id => $field_id }, { email => $email, lastchanged => time });
-   return qq~{"result":"1"}~;
-  }
-  
-  # Check realname
-  
-  if ($field_name eq 'realname') {
-   my $realname=$field_value;
-   $realname=~s/[\<\>\"\'\n\r\\\/]//gm;
-   if ($realname !~ /^.{0,80}$/) {
-    return qq~{"result":"0","error":"~.$lang->{form_error_invalid_realname}.qq~"}~;
-   }
-   database->quick_update(config->{db_table_prefix}.'_billing', { id => $field_id }, { realname => $realname, lastchanged => time });
-   return qq~{"result":"1"}~;
-  }
-
-  # Check phone
-  
-  if ($field_name eq 'phone') {
-   my $phone=$field_value;
-   $phone=~s/[^0-9]//gm;
-   if (length($phone) > 40) {
-    return qq~{"result":"0","error":"~.$lang->{form_error_invalid_phone}.qq~"}~;
-   }
-   database->quick_update(config->{db_table_prefix}.'_billing', { id => $field_id }, { phone => $phone, lastchanged => time });
-   return qq~{"result":"1"}~;
-  }
-  
-  # Check status
-  
-  if ($field_name eq 'status') {
-   my $status=int($field_value);
-   if ($status < 0 || $status > 2) {
-    return qq~{"result":"0","error":"~.$lang->{form_error_invalid_status}.qq~"}~;
-   }
-   if ($taracot::admin::authdata->{id} eq $field_id && $taracot::admin::authdata->{status} eq 2) {
-    return qq~{"result":"0","error":"~.$lang->{form_error_invalid_status}.qq~"}~;
-   }
-   database->quick_update(config->{db_table_prefix}.'_billing', { id => $field_id }, { status => $status, lastchanged => time });
-   return '{"result":"1"}';
-  } 
-
-  return '{"result":"0", "error":"'.$lang->{field_edit_error_unknown}.'"}';
-};
-
 post '/data/load' => sub {
   if (!&taracot::admin::_auth()) { redirect '/admin?'.md5_hex(time); return true }
   _load_lang();
@@ -333,64 +145,58 @@ post '/data/load' => sub {
   if ($id <= 0) {
    return qq~{"result":"0"}~;
   }
-  
-  my $db_data  = database->quick_select(config->{db_table_prefix}.'_billing', { id => $id });
-  
-  if ($db_data->{id}) {
-   $db_data->{result} = 1;
-   $db_data->{password} = 0;
-   my $json_xs = JSON::XS->new();
-   my $json = $json_xs->encode($db_data);
-   return $json;
-  } else {
-   return qq~{"result":"0"}~;
-  }   
-};  
-
-post '/data/delete' => sub {
-  if (!&taracot::admin::_auth()) { redirect '/admin?'.md5_hex(time); return true }
-  _load_lang();
-  content_type 'application/json';
-  
-  my $id=param('delete_data[]') || '';
-  
-  if (length($id) == 0) {
-   return qq~{"result":"0"}~;
+  my %response;
+  my $sth = database->prepare(
+    'SELECT username FROM `'.config->{db_table_prefix}.'_users` WHERE id='.$id
+  );
+  if ($sth->execute()) {
+    my ($username) = $sth -> fetchrow_array();
+    $response{username} = $username;
   }
-
-  my $del_sql;
-  if(ref($id) eq 'ARRAY'){
-   foreach my $item (@$id) {
-    $item=int($item);
-    if ($item ne $$taracot::admin::authdata->{id}) {
-     $del_sql.=' OR id='.$item;
-    }
-   }
-   if ($del_sql) {
-    $del_sql=~s/ OR //;
-   }
-  } else {
-    if ($id ne $taracot::admin::authdata->{id}) {
-     $del_sql='id='.int($id);
-    }
-  }
-  if ($del_sql) {
-    my $sth = database->prepare(
-     'DELETE FROM '.config->{db_table_prefix}.'_billing WHERE '.$del_sql
-    );
-    my $res;
-    if ($sth->execute()) {
-     $res=qq~{"result":"1"}~;
-    } else {
-     $res=qq~{"result":"0"}~;
-    }
-    $sth->finish();
-    return $res;
-  } else {
+  $sth->finish();
+  if (!$response{username}) {
     return qq~{"result":"0"}~;
+  }  
+  my $avatar = '/images/default_avatar.png';
+  if (-e config->{files_dir}.'/avatars/'.$response{username}.'.jpg') {
+    $avatar = config->{files_url}.'/avatars/'.$response{username}.'.jpg';
+  } 
+  $response{avatar} = $avatar;
+  # select funds amount
+  $sth = database->prepare(
+    'SELECT amount FROM `'.config->{db_table_prefix}.'_billing_funds` WHERE user_id='.$id
+  );
+  if ($sth->execute()) {
+    my ($amount) = $sth -> fetchrow_array();
+    $response{amount} = $amount;
   }
-      
-};
+  $sth->finish();
+  if (!$response{amount}) {
+    $response{amount} = '0';
+  }
+  # load hosting plan names from settings
+  
+  # select hosting accounts
+  $sth = database->prepare(
+    'SELECT host_acc,host_plan_id,host_days_remain FROM `'.config->{db_table_prefix}.'_billing_hosting` WHERE user_id='.$id
+  );
+  if ($sth->execute()) {
+    my @host_accounts;
+    while (my ($host_acc,$host_plan_id,$host_days_remain) = $sth -> fetchrow_array()) {
+      my %data;
+      $data{account} = $host_acc;
+      $data{plan_id} = $host_plan_id;
+      $data{days} = $host_days_remain;
+      push (@host_accounts, \%data);
+    }
+    $response{hosting} = \@host_accounts;
+  }
+  $sth->finish();
+  # return data
+  my $json_xs = JSON::XS->new();
+  my $json = $json_xs->encode(\%response);
+  return $json;
+};  
 
 # End
 
