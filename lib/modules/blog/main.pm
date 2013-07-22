b7cbb4c3d5f9421883de62d32f5db00ee66db5ff
mma2 <mma2@SPBWS-PRC293.t-systems.ru>
2013-07-22 19:47:07 +0400

    Blog WIP

--- lib/modules/blog/main.pm
+++ lib/modules/blog/main.pm
@@ -6,6 +6,8 @@ use Digest::MD5 qw(md5_hex);
 use Date::Format;
 use taracot::fs;
 use taracot::AUBBC;
+use URI::Encode qw(uri_encode uri_decode);
+use Encode;
 
 # Configuration
 
@@ -46,60 +48,92 @@ sub flow() {
   my $flow = '';
   my $ipp = 2; # items per page
   my $page = int($_[0]) || 1;
+  my $hub = $_[1] || '';
+  my $tag = $_[2] || '';
   my $_current_lang=_load_lang(); 
   my $aubbc = taracot::AUBBC->new();
   my $auth = &taracot::_auth();
-
+  my $where = 'plang='.database->quote($_current_lang);
+  if ($auth->{id}) {
+    $where.=' AND pstate > 0';
+  } else {
+    $where.=' AND pstate = 1';
+  }
+  if ($hub) {
+    $where.=' AND phub='.database->quote($hub);
+  }
+  if ($tag) {
+    $where.=' AND MATCH (ptags) AGAINST (\''.$tag.'*\' IN BOOLEAN MODE)';
+  }
   my $total=0;
+  open(DATA, '>C:/XTreme/test.sql');
+  print DATA 'SELECT COUNT(*) AS cnt FROM '.config->{db_table_prefix}.'_blog_posts WHERE '.$where;
+  close(DATA);
   my $sth = database->prepare(
-   'SELECT COUNT(*) AS cnt FROM '.config->{db_table_prefix}.'_blog_posts WHERE 1'
+   'SELECT COUNT(*) AS cnt FROM '.config->{db_table_prefix}.'_blog_posts WHERE '.$where
   );
   if ($sth->execute()) {
    ($total) = $sth -> fetchrow_array;
   }
   $sth->finish();
   my $pc = int($total / $ipp);
-  my $pitems='';
-  if ($page ne 1) {
-    $pitems .= template 'blog_paginator_item', { page_url => '/blog/page/1', page_text => '&laquo;' }, { layout => undef };
-  }  
-  my $lof = 5-$page;
-  if ($lof > 4 || $lof < 0) {
-    $lof=0;
+  if ($total % $ipp) {
+    $pc++;
   }
-  my $rof = -($page-$pc);
-  if ($rof > 4) {
-    $rof=0;
-  } else {
-    $rof=4-$rof;
-  }
+  my $paginator='';
+  # Paginator code : begin
+  if ($pc > 1) {
+    my $url = '/blog/page/';
+    if ($hub) {
+      $url='/blog/hub/'.$hub.'/';
+    }
+    if ($tag) {          
+      my $tagurl = uri_encode($tag);
+      $tagurl=~s/%/_/gm;
+      $url='/blog/tag/'.$tagurl.'/';
+    }
+    my $pitems='';
+    if ($page ne 1) {
+      $pitems .= template 'blog_paginator_item', { page_url => $url.'1', page_text => '&laquo;' }, { layout => undef };
+    }    
+    my $lof = 4-$page;
+    if ($lof > 3 || $lof < 0) {
+      $lof=0;
+    }
+    my $rof = -($page-$pc);
+    if ($rof > 3) {
+      $rof=0;
+    } else {
+      $rof=3-$rof;
+    }
 
-  for (my $it=1; $it<=$pc; $it++) {    
-    if ($pc>9) {
-      if ($it < $page-4-$rof || $it > $page+4+$lof) {
-        next;
+    for (my $it=1; $it<=$pc; $it++) {    
+      if ($pc>7) {
+        if ($it < $page-3-$rof || $it > $page+3+$lof) {
+          next;
+        }
       }
+      my $active;
+      if ($it eq $page) {
+        $active = 1;
+      }
+      $pitems .= template 'blog_paginator_item', { page_url => $url.$it, page_text => $it,active => $active }, { layout => undef };
     }
-    my $active;
-    if ($it eq $page) {
-      $active = 1;
+    if ($page ne $pc) {
+      $pitems .= template 'blog_paginator_item', { page_url => $url.$pc, page_text => '&raquo;' }, { layout => undef };
     }
-    $pitems .= template 'blog_paginator_item', { page_url => '/blog/page/'.$it, page_text => $it,active => $active }, { layout => undef };
+    $paginator = template 'blog_paginator', { items => $pitems }, { layout => undef };
   }
-  if ($page ne $pc) {
-    $pitems .= template 'blog_paginator_item', { page_url => '/blog/page/'.$pc, page_text => '&raquo;' }, { layout => undef };
-  }
-  my $paginator = template 'blog_paginator', { items => $pitems }, { layout => undef };
 
   my $limx = $page*$ipp-$ipp;
   $sth = database->prepare(
-   'SELECT id,pusername,phub,ptitle,ptags,pdate,uid,ptext,lastmodified,pviews,is_public FROM '.config->{db_table_prefix}.'_blog_posts WHERE plang='.database->quote($_current_lang).' ORDER BY pdate DESC LIMIT '.$limx.', '.$ipp
+   'SELECT id,pusername,phub,ptitle,ptags,pdate,uid,ptext,lastmodified,pviews,pstate FROM '.config->{db_table_prefix}.'_blog_posts WHERE '.$where.' ORDER BY pdate DESC LIMIT '.$limx.', '.$ipp
   );
   if ($sth->execute()) {
-   while(my ($id,$pusername,$phub,$ptitle,$ptags,$pdate,$uid,$ptext,$lastmodified,$pviews,$is_public) = $sth -> fetchrow_array) {
+   while(my ($id,$pusername,$phub,$ptitle,$ptags,$pdate,$uid,$ptext,$lastmodified,$pviews,$pstate) = $sth -> fetchrow_array) {
     my $phub_url;
     if ($phub) {
-      $phub_url = '/blog/hub/'.$phub;
+      $phub_url = '/blog/hub/'.$phub.'/1';
     }
     if (!$pviews) {
       $pviews='&nbsp;0';
@@ -115,8 +149,11 @@ sub flow() {
     my @tags = split(/,/, $ptags);
     $ptags='';
     foreach my $tag (@tags) {
-      $tag=~s/^ //;      
-      $ptags.=', <a href="/blog/tag/'.$tag.'">'.$tag.'</a>';
+      $tag=~s/^ //; 
+      #tags!!!   
+      my $tagurl = uri_encode($tag);
+      $tagurl=~s/%/_/gm;
+      $ptags.=', <a href="/blog/tag/'.$tagurl.'/1">'.$tag.'</a>';
     }
     $ptags=~s/, //;
     my $item_template = template 'blog_feed', { post_title => $ptitle, blog_hub => $phub, blog_hub_url => $phub_url, blog_text_cut => $ptext, blog_user => $pusername, blog_views => $pviews, blog_tags => $ptags, blog_read_more => $read_more_url, blog_post_url => $post_url, read_more => $lang->{read_more} }, { layout => undef };
@@ -148,6 +185,37 @@ get '/page/:page' => sub {
   pass();
 };
 
+get '/hub/:hub/:page' => sub {  
+  my $bpage = int(params->{page}) || 1;
+  my $bhub = params->{hub};
+  $bhub =~ s/[^\w\n,]//g;
+  if ($bpage<1 || !$bhub) {
+    pass();
+  }
+  my $render_template = &flow($bpage, $bhub);
+  if ($render_template) {
+    return $render_template;
+  }
+  pass();
+};
+
+get '/tag/:tag/:page' => sub {  
+  my $bpage = int(params->{page}) || 1;
+  my $btag = params->{tag};
+  $btag =~ s/[^\w\nА-Яа-я]//g;
+  if ($bpage<1 || !$btag) {
+    pass();
+  }
+  #tags!!!   
+  $btag=~s/_/%/gm;
+  $btag = decode_utf8(uri_decode($btag));  
+  my $render_template = &flow($bpage, undef, $btag);
+  if ($render_template) {
+    return $render_template;
+  }
+  pass();
+};
+
 get '/post/:post_id' => sub {
   my $post_id = params->{post_id};  
   $post_id=~s/[^0-9]//gm; 
@@ -160,21 +228,51 @@ get '/post/:post_id' => sub {
   my $item_template;
   my $page_data= &taracot::_load_settings('site_title,keywords,description', $_current_lang);  
   my $sth = database->prepare(
-   'SELECT id,pusername,phub,ptitle,ptags,pdate,uid,ptext,lastmodified,pviews,is_public FROM '.config->{db_table_prefix}.'_blog_posts WHERE id = '.$post_id.' ORDER BY pdate DESC'
-  );
+   'SELECT id,pusername,phub,ptitle,ptags,pdate,uid,ptext,lastmodified,pviews,pstate FROM '.config->{db_table_prefix}.'_blog_posts WHERE id = '.$post_id.' ORDER BY pdate DESC'
+  );   
+
   if ($sth->execute()) {
-   my ($id,$pusername,$phub,$ptitle,$ptags,$pdate,$uid,$ptext,$lastmodified,$pviews,$is_public) = $sth -> fetchrow_array();
+   my ($id,$pusername,$phub,$ptitle,$ptags,$pdate,$uid,$ptext,$lastmodified,$pviews,$pstate) = $sth -> fetchrow_array();   
+
+
+   if ($pstate eq 2 && !$auth->{id}) {
+    $sth->finish(); 
+    &taracot::_load_settings('site_title,keywords,description', $_current_lang);    
+    return &taracot::_process_template( template 'blog_index', { detect_lang => $detect_lang, head_html => '<link href="'.config->{modules_css_url}.'blog.css" rel="stylesheet" />', lang => $lang, page_data => $page_data, pagetitle => $lang->{module_name}, news_feed => $lang->{error_unauth}, auth_data => $auth  }, { layout => config->{layout}.'_'.$_current_lang } )
+   }
+   if ($pstate eq 0 && $auth->{status} ne 2) {
+    $sth->finish(); 
+    &taracot::_load_settings('site_title,keywords,description', $_current_lang);    
+    return &taracot::_process_template( template 'blog_index', { detect_lang => $detect_lang, head_html => '<link href="'.config->{modules_css_url}.'blog.css" rel="stylesheet" />', lang => $lang, page_data => $page_data, pagetitle => $lang->{module_name}, news_feed => $lang->{error_draft}, auth_data => $auth  }, { layout => config->{layout}.'_'.$_current_lang } )
+   }
+
    my $phub_url;
    if ($phub) {
-     $phub_url = '/blog/hub/'.$phub;
+     $phub_url = '/blog/hub/'.$phub.'/1';
    }
    if (!$pviews) {
      $pviews='&nbsp;0';
    }    
    $ptext =~s/\[cut\]/ /igm;
    $ptext = $aubbc->do_all_ubbc($ptext);
+   $ptags =~ s/[^\w\n ,]//g;
+    my @tags = split(/,/, $ptags);
+    $ptags='';
+    foreach my $tag (@tags) {
+      $tag=~s/^ //;   
+      #tags!!!   
+      my $tagurl = uri_encode(substr($tag,1,20));   
+      $tagurl=~s/%/_/gm;
+      $ptags.=', <a href="/blog/tag/'.$tagurl.'/1">'.$tag.'</a>';
+    }
+    $ptags=~s/, //;
    $item_template = &taracot::_process_template( template 'blog_post', { detect_lang => $detect_lang, head_html => '<link href="'.config->{modules_css_url}.'blog.css" rel="stylesheet" />', lang => $lang, page_data => $page_data, pagetitle => $ptitle.' | '.$lang->{module_name}, post_title => $ptitle, blog_hub => $phub, blog_hub_url => $phub_url, blog_text => $ptext, blog_user => $pusername, blog_views => $pviews, blog_tags => $ptags, auth_data => $auth }, { layout => config->{layout}.'_'.$_current_lang } );
   }
+  $sth->finish(); 
+  $sth = database->prepare(
+   'UPDATE '.config->{db_table_prefix}.'_blog_posts SET pviews=pviews+1 WHERE id = '.$post_id
+  );
+  $sth->execute();
   $sth->finish(); 
   if ($item_template) {
     return $item_template;
