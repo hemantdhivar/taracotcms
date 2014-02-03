c27bb1a4e97803ffa0afb9de30aca824a9a4fabb
XTreme <XTreme@localhost>
2014-02-03 19:01:13 +0400

    Social module WIP, updated Bootstrap to 3.1.0

--- public/js/modules/social_index.js
+++ public/js/modules/social_index.js
@@ -59,7 +59,8 @@ var ajax_load_search_data = function(query, page) {
                        $('#social_search_results').append('<div class="col-lg-6 col-md-12 col-sm-12 col-xs-12 social-search-item" id="social_search_item_' + data.items[i].id + '"><div class="media social-search-media" style="margin:5px 0 5px 0"><span class="pull-left"><img class="media-object" src="'+data.items[i].avatar+'" alt="'+h1t+'" style="width:50px"></span><div class="media-body"><h4 class="media-heading">'+h1t+'</h4>'+h2t+'</div></div></div>');
                     }
                     $('#social_search_results').append('</div>');
-                    $('.social-search-item').click(social_search_click_event);                    
+                    $('.social-search-item').unbind();
+                    $('.social-search-item').click(social_search_click_event);
                 }
                 if (social_search_page == 1) {
                 	$("html, body").animate({ scrollTop: 0 }, "slow");
@@ -85,8 +86,8 @@ $('#social_search_btn').click(function () {
 		$('#social_cg_search').addClass('has-error');
 		return;
 	}
-    ajax_load_search_data($('#social_search_input').val(), 1);
-
+  ajax_load_search_data($('#social_search_input').val(), 1);
+  $.history.push("view=search&query=" + $('#social_search_input').val() );
 });
 $(window).scroll(function() {
    if($(window).scrollTop() + $(window).height() > $(document).height() - 100 && social_search_page > 0) {
@@ -97,6 +98,7 @@ $(window).scroll(function() {
 var ajax_load_user_data = function(uid) {
 	$('#social_page_ajax').show();
 	$('#social_page_results').html('');
+  console.log('ajax_load_user_data called, id='+uid);
 	$.ajax({
         type: 'POST',
         url: '/social/user',
@@ -107,9 +109,12 @@ var ajax_load_user_data = function(uid) {
         success: function (data) {
         	$('#social_page_ajax').hide();
             if (data.status == 1) {    
-            	$('#social_page_results').append('<h1>' + data.realname + '</h1>');
+            	$('#social_page_results').append('<h1 class="social_page_header">' + data.realname + '<img src="' + data.avatar +'" class="pull-right" alt="' + data.realname + '"></h1>');
+              if (uid != current_user_id) {
+                $('#social_page_results').append('<button class="btn btn-success"><i class="glyphicon glyphicon-plus"></i>&nbsp;' + js_lang_add_friend + '</button>');
+              }
             } else {
-                $('#social_page_results').html('<div class="alert alert-danger">'+js_lang_page_error+'</div>');
+              $('#social_page_results').html('<div class="alert alert-danger">'+js_lang_page_error+'</div>');
             }
         },
         error: function () {
@@ -129,19 +134,73 @@ var social_search_click_event = function(uid) {
 	$('.social_tab').hide();
 	social_search_page = -1;
 	$('#social_tab_page').show();
-  $.history.push("user_details");
+  $.history.push("view=page&id=" + id );
 	ajax_load_user_data(id);	
 };
-$.history.on('change', function(event, url, type) {
-  // alert(url);
-  if (url == 'search_results') {
+$('.social_menu_link').click(function(e) {
+  $.history.push($(this).attr('href'));
+  e.preventDefault();
+});
+$.history.on('load change', function(event, url, type) {
+  var items = url.split('&');
+  var ph = $.unparam(url);  
+  if (ph['view'] == 'search_results') {
     $('.social_tab').hide();    
     $('#social_tab_search').show();
+    return;
   }
-  if (url == 'my_page' || url == '') {
+  social_search_page = -1;
+  if (ph['view'] == 'page' || ph['view'] == '') {
     $('.social_menu').removeClass('active');
     $('.social_tab').hide();
-    $('#social_tab_page').show();
-    $('#social_menu_page_li').addClass('active');
+    $('#social_tab_page').show();    
+    if (ph['id'] && ph['id'].length > 0) {
+      ajax_load_user_data(ph['id']);
+    } else {
+      $('#social_menu_page_li').addClass('active');
+      ajax_load_user_data(current_user_id);
+    }
+    return;
   }
+  if (ph['view'] == 'friends') {
+    $('.social_menu').removeClass('active');
+    $('.social_tab').hide();
+    $('#social_tab_friends').show();
+    $('#social_menu_friends_li').addClass('active');
+    return;
+  }
+  if (ph['view'] == 'search') {
+    $('.social_menu').removeClass('active');
+    $('.social_tab').hide();
+    $('#social_tab_search').show();
+    $('#social_search_results').html('');
+    $('#social_menu_search_li').addClass('active');
+    if (ph['query'] && ph['query'].length > 0) {
+      $('#social_search_input').val(ph['query']);
+      $('#social_search_btn').click();      
+    }    
+    $('#social_search_input').focus();
+    return;
+  }
+  if (ph['view'] == 'invitations') {
+    $('.social_menu').removeClass('active');
+    $('.social_tab').hide();
+    $('#social_tab_invitations').show();
+    $('#social_menu_invitations_li').addClass('active');
+    return;
+  }
+  if (ph['view'] == 'messaging') {
+    $('.social_menu').removeClass('active');
+    $('.social_tab').hide();
+    $('#social_tab_messaging').show();
+    $('#social_menu_messaging_li').addClass('active');
+    return;
+  }
+  if (ph['view'] == 'settings') {
+    $('.social_menu').removeClass('active');
+    $('.social_tab').hide();
+    $('#social_tab_settings').show();
+    $('#social_menu_settings_li').addClass('active');
+    return;
+  }  
 }).listen('hash');
